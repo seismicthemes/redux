933d92526e81b1e2afa53d86d0c67a90266bc454
bmcculley <zen.fed96@gmail.com>
2014-04-01 23:05:53 -0400

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Redux
 =============
 
 The WordPress starter theme brought back.
+
+![Redux](http://i.imgur.com/4qxfQUV.png)
---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+?><!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed">
+<?php do_action( 'before' ); ?>
+	<header id="branding" role="banner">
+		<hgroup>
+			<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
+			<?php $description_tag = ( is_home() || is_front_page() ) ? 'h2' : 'div'; ?>
+			<<?php echo $heading_tag; ?> id="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></<?php echo $heading_tag; ?>>
+			<<?php echo $description_tag; ?> id="site-description"><?php bloginfo( 'description' ); ?></<?php echo $description_tag; ?>>
+		</hgroup>
+
+		<nav id="access" class="main-navigation" role="navigation">
+			<h1 class="assistive-text section-heading"><?php _e( 'Main menu', 'toolbox' ); ?></h1>
+			<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'toolbox' ); ?>"><?php _e( 'Skip to content', 'toolbox' ); ?></a></div>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'fallback_cb' => false, 'depth' => 4 ) ); ?>
+		</nav><!-- #access -->
+	</header><!-- #branding -->
+
+	<div id="main">
